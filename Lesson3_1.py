eaa33da18acf866e5c3290dc9d7eb326f10c39e9
FuroFag <68804043+FuroFag@users.noreply.github.com>
2024-08-22 18:42:48 +0400

    Update Lesson3_1.py

--- Lesson3_1.py
+++ Lesson3_1.py
@@ -5,10 +5,11 @@ def count_calls():
 def string_info(string):
     count_calls()
     return (len (string), string.upper(), string.lower())
-def is_contain(string, list_to_search):
-    return string.casefold() in [s.casefold for s in list_to_search]
+def is_contain(string, da_prostit_menya_Bog):
+    string_info.upper()
+    
 print (string_info('DINNER'))
 print (string_info('GIRL HELL 1999'))
-print (is_contain('Katamari', ['KAtAmari', 'amar', 'maramaarmaam']))
+print (is_contain('Katamari', ['Katamari', 'amar', 'maramaarmaam']))
 print (is_contain('S33KH3LP', ['S33KH3LP', 'SEEKHELP', 'S33K HELP']))
 print (calls)
