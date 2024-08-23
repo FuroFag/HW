9f47abe17e7d0dc5975dd7e12b6c24549df8c5c2
FuroFag <68804043+FuroFag@users.noreply.github.com>
2024-08-23 13:49:18 +0400

    Update Lesson3_1.py

--- Lesson3_1.py
+++ Lesson3_1.py
@@ -6,10 +6,10 @@ def string_info(string):
     count_calls()
     return (len (string), string.upper(), string.lower())
 def is_contain(string, da_prostit_menya_Bog):
-    string_info.upper()
-    
+    count_calls()
+    return string.upper() in [s.upper() for s in da_prostit_menya_Bog]
 print (string_info('DINNER'))
 print (string_info('GIRL HELL 1999'))
-print (is_contain('Katamari', ['Katamari', 'amar', 'maramaarmaam']))
+print (is_contain('Katamari', ['KataMari', 'amar', 'maramaarmaam']))
 print (is_contain('S33KH3LP', ['S33KH3LP', 'SEEKHELP', 'S33K HELP']))
 print (calls)
