fe8187c4e3fa950f874e8e687915259be763d9d2
FuroFag <68804043+FuroFag@users.noreply.github.com>
2024-09-10 11:12:06 +0400

    Create Lesson4_1.py

---
+++ Lesson4_1.py
@@ -0,0 +1,11 @@
+from True_Math import Divide
+from Fake_Math import Divide as Divide2
+
+result1 = Divide2(21, 7)
+print(result1)
+result2 = Divide(1000032, 2)
+print(result2)
+result3 = Divide(10, 0)
+print(result3)
+result4 = Divide2(10, 0)
+print(result4)
