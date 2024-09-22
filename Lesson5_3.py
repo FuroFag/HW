42fc7ead7df59b7d326b56627b5c59698d04bc93
FuroFag <68804043+FuroFag@users.noreply.github.com>
2024-09-22 12:17:40 +0400

    Create Lesson5_3.py

---
+++ Lesson5_3.py
@@ -0,0 +1,85 @@
+class House:
+    def __init__(self ,name, nof):
+        self.name = name
+        self.nof = nof
+
+    def go_to(self, new_floor):
+        for i in range(1, new_floor + 1):
+            if 1 <= new_floor <= self.nof:
+                print(i)
+            else:
+                print('Такого этажа нет')
+                break
+
+    def __len__(self):
+        return self.nof
+
+    def __str__(self):
+        return (f'{self.name} {self.nof}')
+
+    def __eq__(self, other):
+        if self.nof == other.nof:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if self.nof < other.nof:
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        if self.nof <= other.nof:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        if self.nof > other.nof:
+            return True
+        else:
+            return False
+
+    def __ge__(self, other):
+        if self.nof >= other.nof:
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        if self.nof != other.nof:
+            return True
+        else:
+            return False
+
+    def __add__(self, value):
+        self.nof = self.nof + value
+        return self.nof
+
+    def __radd__(self, value):
+        self.nof = self.nof + value
+        return self.nof
+
+    def __iadd__(self, value):
+        self.nof = self.nof + value
+        return self.nof
+
+h1 = House('ЖК Эльбрус', 30)
+h2 = House('ЖК Приречный', 9)
+print (len(h1))
+print (str(h1))
+print (len(h2))
+print (str(h2))
+print (h1 == h2)
+print (h1 < h2)
+print (h1 <= h2)
+print (h1 > h2)
+print (h1 >= h2)
+print (h1 != h2)
+h1 = h1 + 11
+print (h1)
+h2 = h2 + 2
+print (h2)
+h1 = h1 + 1
+print (h1)
