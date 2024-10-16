8b0c419b07e8e4d40d548d6bb2b740deb4735df9
FuroFag <68804043+FuroFag@users.noreply.github.com>
2024-10-16 12:42:54 +0400

    Update Lesson7_2.py

--- Lesson7_2.py
+++ Lesson7_2.py
@@ -3,18 +3,14 @@ import io
 
 def custom_write(file_name, strings):
     strings_positions = {}
-
+    counter = 1
     file = open(file_name, 'w', encoding = 'utf-8')
     for i in strings:
         byte = file.tell()
         file.write(f'{i}\n')
-    file.close()
-    file = open(file_name, 'r')
-    counter = 1
-    for i in file:
         strings_positions[(counter, byte)] = i.replace('\n', '')
         counter += 1
     file.close()
-    return strings_positions
+    print (strings_positions)
 
 custom_write('stuff.txt', ['i cant breathe', 'officer', 'george floyd'])
