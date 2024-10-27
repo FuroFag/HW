8a08923abf04bcb7bfc3bc53e20a3a9d59bd62eb
FuroFag <68804043+FuroFag@users.noreply.github.com>
2024-10-27 13:54:23 +0400

    Create Lesson9_2.py

---
+++ Lesson9_2.py
@@ -0,0 +1,12 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+a = ()
+result1 = [x for x in first_strings if len(x) >= 5]
+
+result2 = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+full_string = first_strings + second_strings
+result3 = [{x: len(x)} for x in full_string if len(x) %2 == 0]
+
+print (result1)
+print (result2)
+print (result3)
