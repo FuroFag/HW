858ad9ba133340c87b41093a64ef5c11be2d1800
FuroFag <68804043+FuroFag@users.noreply.github.com>
2024-10-27 14:40:53 +0400

    Update Lesson9_2.py

--- Lesson9_2.py
+++ Lesson9_2.py
@@ -1,7 +1,7 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 a = ()
-result1 = [x for x in first_strings if len(x) >= 5]
+result1 = [len(x) for x in first_strings if len(x) >= 5]
 
 result2 = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
 full_string = first_strings + second_strings
