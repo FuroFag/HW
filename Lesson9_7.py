c6fefd6106be1987c99e15820961f0fecab48232
FuroFag <68804043+FuroFag@users.noreply.github.com>
2024-11-12 12:10:27 +0400

    Update Lesson9_7.py

--- Lesson9_7.py
+++ Lesson9_7.py
@@ -1,14 +1,18 @@
 def is_prime(func):
     def wrapper(*args):
         summed = func(*args)
+        prime = True
         if summed > 1:
             for i in range(2, (summed//2) + 1):
                 if (summed % i) == 0:
-                    return summed, 'Не простое'
-                else:
-                    return summed, 'Простое'
+                    prime = False
+                    break
+            if prime == True:
+                return f'{summed} Простое'
+            else:
+                return f'{summed} Не простое'
         else:
-            return summed, 'Не простое'
+            return f'{summed} Простое'
     return wrapper
 
 @is_prime
@@ -19,4 +23,4 @@ def sum_three(*args):
     result = sum(_list)
     return result
 
-print(sum_three(1, 0, 11))
+print(sum_three(1, 0, 10))
