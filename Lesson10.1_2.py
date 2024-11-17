7ce9abd4d9d85e4a8059a7d97aa1e169ce5e68b0
FuroFag <68804043+FuroFag@users.noreply.github.com>
2024-11-17 17:24:45 +0400

    Update Lesson10.1_2.py

--- Lesson10.1_2.py
+++ Lesson10.1_2.py
@@ -3,26 +3,26 @@ from threading import Thread
 import time
 
 class Knight(Thread):
-    def __init__(self, name, power):
+    def __init__(self, name, power, enemies):
         threading.Thread.__init__(self)
         self.name = name
+        self.enemies = enemies
         self.power = power
 
-    def run(self, enemies):
+    def run(self):
         print(f'{self.name}, на нас напали!')
         day = 0
-        while enemies > 0:
-            enemies -= self.power
+        while self.enemies > 0:
+            self.enemies -= self.power
             time.sleep (1)
             day += 1
-            print (f'{self.name} сражается {day}, осталось {enemies} врагов')
+            print (f'{self.name} сражается {day}, осталось {self.enemies} врагов')
         print (f'{self.name} одержал победу спустя {day}дней(дня)')
 
 
-knight1 = Knight('George Floyd', 10)
-knight1.run(110)
-knight2 = Knight('Lebron James', 5)
-knight2.run(25)
+knight1 = Knight('George Floyd', 10, 100)
+knight2 = Knight('Lebron James', 5, 20)
 knight1.start()
 knight2.start()
-
+knight1.join()
+knight2.join()
