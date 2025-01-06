29afe66fece941ddc6a822ac77f408c7588f97bf
FuroFag <68804043+FuroFag@users.noreply.github.com>
2025-01-06 17:26:09 +0400

    Update Lesson13_2.py

--- Lesson13_2.py
+++ Lesson13_2.py
@@ -3,15 +3,17 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 from aiogram.utils import executor
 
-api = ''
+api = '7641778786:AAEN89Ovn4x0N4sFAz5PKxEAqIlae7zpbaQ'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
+
 @dp.message_handler(commands=['start'])
 async def start(message):
-    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
 @dp.message_handler()
 async def all_message(message):
-    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
